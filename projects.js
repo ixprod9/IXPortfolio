963b5b71eb9922c618aeb7e28bb17542d2e81bde
IXProd <42524726+ixprod9@users.noreply.github.com>
2026-02-24 17:12:39 -0500

    Add files via upload

--- projects.js
+++ projects.js
@@ -7,11 +7,7 @@ window.PROJECT_DATA = [
             "https://cdna.artstation.com/p/assets/images/images/084/222/592/large/ix-productions-nvidia-overlay-4zi0a0owi6.jpg?1737839437",
             "https://www.youtube.com/watch?v=Pd_uUQpoZzw",
             "https://cdna.artstation.com/p/assets/images/images/084/222/596/large/ix-productions-nvidia-overlay-foxwnzoypd.jpg?1737839440",
-            "https://cdna.artstation.com/p/assets/images/images/084/222/610/large/ix-productions-nvidia-overlay-du8x8qokyo.jpg?1737839463",
-            "https://cdna.artstation.com/p/assets/images/images/084/222/606/large/ix-productions-nvidia-overlay-w9xo8yustt.jpg?1737839459",
-            "https://cdna.artstation.com/p/assets/images/images/084/222/598/large/ix-productions-nvidia-overlay-zjahqpjwkj.jpg?1737839444",
-            "https://cdnb.artstation.com/p/assets/images/images/084/222/601/large/ix-productions-nvidia-overlay-gxavgraszx.jpg?1737839448",
-            "https://cdnb.artstation.com/p/assets/images/images/084/222/605/large/ix-productions-nvidia-overlay-bny0xwlo9y.jpg?1737839454"
+            "https://cdna.artstation.com/p/assets/images/images/084/222/610/large/ix-productions-nvidia-overlay-du8x8qokyo.jpg?1737839463"
         ],
         software: ["Blender", "Substance 3D Painter", "DaVinci Resolve", "Nuke"],
         description: "Cute little animation i wanted to do with a Katamari Prince model made, clay prince is cute. Made using Blender, Prince model made by me, lighting, set dressing, rendering, final image, all by me. Environment assets by Quixel Megascans",
@@ -19,12 +15,31 @@ window.PROJECT_DATA = [
     },
     {
         id: "02",
-        title: "City_Ruins_Environment",
-        type: "image",
-        mediaUrl: "assets/project2_render.jpg",
-        software: ["Unreal Engine 5", "SpeedTree", "Quixel"],
-        description: "An exploration of post-apocalyptic nature reclamation using nanite and lumen.",
-        link: "https://github.com/yourname"
+        title: "Outlander_From_The_Farlands",
+        type: "mixed",
+        mediaUrl: [
+            "https://cdnb.artstation.com/p/assets/images/images/084/224/265/large/ix-productions-outlander1.jpg?1737844281",
+            "https://youtu.be/KDS-IAyqJlo",
+            "https://cdna.artstation.com/p/assets/images/images/084/224/682/large/ix-productions-posterf1.jpg?1737845552",
+            "https://cdn.artstation.com/p/video_sources/002/402/639/8mb.mp4",
+            "https://cdna.artstation.com/p/assets/images/images/084/224/328/large/ix-productions-nvidia-overlay-ymcgrz1zoi.jpg?1737844450",
+            "https://cdna.artstation.com/p/assets/images/images/084/224/364/large/ix-productions-nvidia-overlay-vzcllanj3o.jpg?1737844497",
+            "https://cdna.artstation.com/p/assets/images/images/084/224/330/large/ix-productions-nvidia-overlay-qcff3vrjep.jpg?1737844455",
+            "https://cdnb.artstation.com/p/assets/images/images/084/224/337/large/ix-productions-nvidia-overlay-tfzqntjchu.jpg?1737844460",
+            "https://cdna.artstation.com/p/assets/images/images/084/224/340/large/ix-productions-nvidia-overlay-dauijyaaud.jpg?1737844464",
+            "https://cdnb.artstation.com/p/assets/images/images/084/224/321/large/ix-productions-unrealeditor-dstooxxwhg.jpg?1737844439",
+            "https://cdnb.artstation.com/p/assets/images/images/084/224/325/large/ix-productions-unrealeditor-jqdbnwxuue.jpg?1737844443",
+            "https://cdn.artstation.com/p/video_sources/002/402/637/whatsapp-video-2024-05-02-at-9.mp4",
+            "https://cdnb.artstation.com/p/assets/images/images/084/224/361/large/ix-productions-nvidia-overlay-lqqcvliluf.jpg?1737844493",
+            "https://cdna.artstation.com/p/assets/images/images/084/224/352/large/ix-productions-nvidia-overlay-oyyvjitud6.jpg?1737844479",
+            "https://cdna.artstation.com/p/assets/images/images/084/224/356/large/ix-productions-nvidia-overlay-rjfb4wgwak.jpg?1737844484",
+            "https://cdnb.artstation.com/p/assets/images/images/084/224/349/large/ix-productions-nvidia-overlay-yjjjyfl6aa.jpg?1737844475",
+            "https://cdna.artstation.com/p/assets/images/images/084/224/344/large/ix-productions-nvidia-overlay-qv6v94krsj.jpg?1737844468",
+            "https://cdnb.artstation.com/p/assets/images/images/084/224/347/large/ix-productions-nvidia-overlay-hqio2p3nee.jpg?1737844471"
+        ],
+        software: ["Unreal Engine 5", "Blender", "Substance 3D Painter", "Nuke", "DaVinci Resolve"],
+        description: "Short Film, made for a college project in 2024 in Unreal Engine | Animation, Texture & Material Work, Modelling, Character Design - Me / IX Productions Audio- Red Dead Redemption 2 / Rockstar Games Walk Cycle Animation- Mixamo Environment Assets - Quixel Retro Teddy bear 3D Scan - matousekfoto / sketchfab Realistic Starter VFX Pack Vol 2 - FX Cat UA.",
+        link: "https://www.artstation.com/artwork/KDS-IAyqJlo"
     },
     {
         id: "03",
@@ -38,7 +53,7 @@ window.PROJECT_DATA = [
             "https://cdna.artstation.com/p/assets/images/images/061/627/396/large/ix-productions-image.jpg?1681255548"
         ],
         software: ["Blender"],
-        description: "Bit of geometry nodes experimenting, this time around i built a procedural knitting system that uses the mesh UVS to map itself into the original object's shape, this is the same method used on the official LEGO movies but sadly its too computing heavy to use on a day-to-day basis for a regular person.",
+        description: "Procedural knitting system using geometry nodes and UV mapping.",
         link: "https://www.artstation.com/artwork/Xg9V03"
     }
 ];

b12a43ebe0f0c423ee85c9f1432a407a8d59f429
IXProd <42524726+ixprod9@users.noreply.github.com>
2026-02-24 16:39:25 -0500

    Add files via upload

---
+++ projects.js
@@ -0,0 +1,44 @@
+window.PROJECT_DATA = [
+    {
+        id: "01",
+        title: "Katamari_Damacy_FanArt_Animation",
+        type: "mixed", 
+        mediaUrl: [
+            "https://cdna.artstation.com/p/assets/images/images/084/222/592/large/ix-productions-nvidia-overlay-4zi0a0owi6.jpg?1737839437",
+            "https://www.youtube.com/watch?v=Pd_uUQpoZzw",
+            "https://cdna.artstation.com/p/assets/images/images/084/222/596/large/ix-productions-nvidia-overlay-foxwnzoypd.jpg?1737839440",
+            "https://cdna.artstation.com/p/assets/images/images/084/222/610/large/ix-productions-nvidia-overlay-du8x8qokyo.jpg?1737839463",
+            "https://cdna.artstation.com/p/assets/images/images/084/222/606/large/ix-productions-nvidia-overlay-w9xo8yustt.jpg?1737839459",
+            "https://cdna.artstation.com/p/assets/images/images/084/222/598/large/ix-productions-nvidia-overlay-zjahqpjwkj.jpg?1737839444",
+            "https://cdnb.artstation.com/p/assets/images/images/084/222/601/large/ix-productions-nvidia-overlay-gxavgraszx.jpg?1737839448",
+            "https://cdnb.artstation.com/p/assets/images/images/084/222/605/large/ix-productions-nvidia-overlay-bny0xwlo9y.jpg?1737839454"
+        ],
+        software: ["Blender", "Substance 3D Painter", "DaVinci Resolve", "Nuke"],
+        description: "Cute little animation i wanted to do with a Katamari Prince model made, clay prince is cute. Made using Blender, Prince model made by me, lighting, set dressing, rendering, final image, all by me. Environment assets by Quixel Megascans",
+        link: "https://www.artstation.com/artwork/yD94XQ"
+    },
+    {
+        id: "02",
+        title: "City_Ruins_Environment",
+        type: "image",
+        mediaUrl: "assets/project2_render.jpg",
+        software: ["Unreal Engine 5", "SpeedTree", "Quixel"],
+        description: "An exploration of post-apocalyptic nature reclamation using nanite and lumen.",
+        link: "https://github.com/yourname"
+    },
+    {
+        id: "03",
+        title: "LEGO_Cape_Weave_Blender_Geometry_Nodes_Prototype",
+        type: "image", 
+        mediaUrl: [
+            "https://cdnb.artstation.com/p/assets/images/images/061/627/389/large/ix-productions-firefox-jmppscidaw.jpg?1681255538",
+            "https://cdnb.artstation.com/p/assets/images/images/061/627/383/large/ix-productions-firefox-nvrnvt4v7w.jpg?1681255526",
+            "https://cdnb.artstation.com/p/assets/images/images/061/627/387/large/ix-productions-firefox-0xj00uuf9j.jpg?1681255531",
+            "https://cdna.artstation.com/p/assets/images/images/061/627/392/large/ix-productions-firefox-c37pbjejfs.jpg?1681255542",
+            "https://cdna.artstation.com/p/assets/images/images/061/627/396/large/ix-productions-image.jpg?1681255548"
+        ],
+        software: ["Blender"],
+        description: "Bit of geometry nodes experimenting, this time around i built a procedural knitting system that uses the mesh UVS to map itself into the original object's shape, this is the same method used on the official LEGO movies but sadly its too computing heavy to use on a day-to-day basis for a regular person.",
+        link: "https://www.artstation.com/artwork/Xg9V03"
+    }
+];
